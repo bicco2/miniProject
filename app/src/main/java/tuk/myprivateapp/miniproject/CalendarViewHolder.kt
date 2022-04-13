497b84a52b0792b0fa2c7fb9fc7b96809856439a
jinhobyeong <jhb_0921@naver.com>
2022-04-13 23:18:34 +0900

    feat : blankEvent error fix

--- app/src/main/java/tuk/myprivateapp/miniproject/CalendarViewHolder.kt
+++ app/src/main/java/tuk/myprivateapp/miniproject/CalendarViewHolder.kt
@@ -12,7 +12,9 @@ class CalendarViewHolder internal constructor(itemView: View, onItemListener: Ca
     private val onItemListener: CalendarAdapter.OnItemListener
     override fun onClick(view: View) {
         //여기서 애댑터 포지션으로 달력 일수 위치 인덱스 정해주고 , dayofmonth에 일수 저장한 다음에 text로 빼서 그걸 mainactivity에서 받아서 변수를 사용하고 자세한 함수 구현하고 여기서 실행
-        onItemListener.onItemClick(adapterPosition, dayOfMonth.text as String)
+        if(dayOfMonth.text != "") {
+            onItemListener.onItemClick(adapterPosition, dayOfMonth.text as String)
+        }
     }
     //기본생성
     init {
